9452692c0b4b47ddb43c1896e934e05abcb5abdb
Shruthi Sridhar <shruthisridhar96@gmail.com>
2021-09-29 21:38:21 +0530

    News API Assignment

--- js/script.js
+++ js/script.js
@@ -1,45 +1,60 @@
+/*News Options Array */
+const newsOptions=['all news','national','business','sports','world','politics','technology','startup','entertainment','miscellaneous','hatke','science','automobile'];
+ /*Getting news by category*/
+async function newsByCategory(category){
+    try{
+        const response=await fetch(`https://inshortsapi.vercel.app/news?category=${category}`);
+        const newsData=await response.json();
+        return newsData;
+       }catch(error){
+           errorHandling(error);
+       }
+ }
+ /*Displaying the news on the DOM */
+ async function displayNews(category){
+    try{
+         const news=category==='all news'?await newsByCategory('all'):await newsByCategory(category);
+         document.querySelector('.CategoryName').innerHTML=category;
+         const newsItems=news.data.map((element)=>{
+                             let item=document.createElement('div');
+                             item.className="NewsItem";
+                             item.insertAdjacentHTML('beforeend',`<div class="card CardItem">
+                                                                 <div class="row">
+                                                                  <div class="col-md-8">
+                                                                  <div class="card-body">
+                                                                  <h4 class="card-title">${element.title}</h4>
+                                                                  <p class="card-text"><small>Posted by ${element.author} on ${element.date} at ${element.time}</small></p>
+                                                                  <div class="card-text content">${element.content}<br> <a href="${element.readMoreUrl}" target="_blank">Read More</a><br><a href="${element.url}" target="_blank">Link on InShorts</a><br>
+                                                                  </div>
+                                                                  </div>
+                                                                  </div>
+                                                                  <div class="col-md-4">
+                                                                  <img src="${element.imageUrl}" class="img-fluid rounded-start" alt="Article Image">
+                                                                  </div>
+                                                                  </div></div><br>`);
+                              return item;
+                          });
+         document.querySelector('.News').replaceChildren(...newsItems);
 
- /*Getting the table*/
- let DataTable=document.querySelector('.data-table');
- /*Getting a Fox Image */
- async function getFoxImage(){
-   try{
-   const response=await fetch('https://randomfox.ca/floof/');
-   const data=await response.json();
-   return data;
-   }
-   catch(error){
-     document.querySelector('.error-message').append(`${error}`);
-   }
- }
+    }catch(error){
+        errorHandling(error);
  
- /*Add a Fox Image */
- document.querySelector('.displayButton').addEventListener('click',addFoxImage);
- async function addFoxImage(){
-   try{
-    let responseData=await getFoxImage();
-    DataTable.insertAdjacentHTML('beforeend',`<tr><td>${getRowIndex()}</td><td><img src="${responseData.image}" alt="Fox Image" height="130" width="130"></td><td><button class="updateButton" onclick="updateFoxImage(this.parentNode.parentNode)">Update Image</button></td><td><button class="deleteButton" onclick="deleteFoxImage(this.parentNode.parentNode)">Delete Image</button></td></tr>`);
-   }
-   catch(error){
-    document.querySelector('.error-message').append(`  ${error}`);
-   }
-  }
- /*Update a Fox Image */
- async function updateFoxImage(row){
-   try{
-  let responseData=await getFoxImage();
-   row.childNodes[1].firstChild.src=responseData.image;
-   }
-   catch(error){
-    document.querySelector('.error-message').append(`  ${error}`);
-   }
- }
- /*Delete a Fox Image */
-function deleteFoxImage(row){
-   DataTable.deleteRow(row.rowIndex);
-   if(DataTable.rows.length>=2){
-   DataTable.rows[getRowIndex()-1].firstChild.innerHTML=getRowIndex()-1;
-   }
 }
-/*Update the row Index value on the table*/
-let getRowIndex=()=>DataTable.rows.length;
+ }
+/*Displaying the news by category */
+function displayByCategory(){
+    const categoryValue=document.querySelector('.Category').value.toLowerCase()==='all'? 'all news': document.querySelector('.Category').value.toLowerCase();
+    if(newsOptions.findIndex((value)=>value===categoryValue)!==-1){
+    displayNews(categoryValue);
+    }
+    else{
+        alert('Please enter a valid option');
+    }
+    document.querySelector('.newsForm').reset();
+}
+/*Error Handling */
+const errorHandling=(error)=>{
+                        document.querySelector('.error-message').innerHTML=error;
+                    };
+/*Displaying all the news by default*/
+displayNews('all news');
